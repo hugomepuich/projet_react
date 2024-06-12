33bc9c0520ea2e39dc989e68cc06fb7fabf54931
hugomepuich <hugo@macbook-pro-de-hugo.home>
2024-06-12 08:27:04 +0200

    header, no style

---
+++ maison-horrifique/src/header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import NavBar from './navbar/NavBar'
+
+function Header () {
+    return (
+        <header id="main-header">
+            <h1>Maison Horrifique</h1>
+            <NavBar />
+        </header>
+    )
+}
+
+export default Header;
---
+++ maison-horrifique/src/header/navbar/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <nav id="main-nav">
+
+                <h3 className='nav-button'>Home</h3>
+                <h3 className='nav-button'>Home</h3>
+                <h3 className='nav-button'>Home</h3>
+                <h3 className='nav-button'>Home</h3>
+        </nav>
+    )
+}
+
+export default NavBar;
